9c2b2bf8c5fb57d0d45a09d365f700850da792c4
y.watanabe <yuki.watanabe@supership.jp>
2020-05-15 18:21:43 +0900

    fixed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,5 @@ Rails.application.routes.draw do
   root "top_pages#top"
   post "top_pages/search_keyword"
   post "top_pages/search_category"
-  get "top_pages/search_keyword"
-  get "top_pages/search_category"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
